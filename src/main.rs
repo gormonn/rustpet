03bb7f96a41ea49316004e217b7c211769e0ceac
Dmitrii Chernukho <d.chernukho@dats.team>
2025-02-01 22:26:57 +0700

    add guessing game

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Угадай число!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    loop {
+        println!("Пожалуйста введи число!");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok (num) => num,
+            Err (_) => continue
+        };
+
+        println!("Ты загадал : {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                println!("Секретное число: {secret_number}");
+                break;
+            },
+        }
+    }
 }
